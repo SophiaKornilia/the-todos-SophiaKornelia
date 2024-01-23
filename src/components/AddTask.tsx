8a6d932d19be65bb761e45b0dbb7a3dbf768f5c6
Kornelia Adabugday <seadabugday@gmail.com>
2024-01-23 18:21:17 +0100

    form

--- src/components/AddTask.tsx
+++ src/components/AddTask.tsx
@@ -1,23 +1,24 @@
 import { ChangeEvent, useState } from "react";
 
 interface IAddTaskProps {
-    addTask: (theNewTask: string) => void; 
+    addTask: (theNewTask: string) => void;
 }
 
 export const AddTask = (props: IAddTaskProps) => {
-    const [newTaskName, setNewTaskName] = useState(""); 
+    const [newTaskName, setNewTaskName] = useState("");
 
-        const handleClick = () => {
-            props.addTask(newTaskName); 
-        }
+    const handleClick = () => {
+        props.addTask(newTaskName);
+    }
 
-        const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-            setNewTaskName(e.target.value)
-        }
-    return ( 
-        <>
-        <input type="text" value={newTaskName} onChange={handleChange} />
-        <button onClick={handleClick}>Save</button>
-            </>
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setNewTaskName(e.target.value)
+    }
+    return (
+        <form>
+            <label>Add a new task:</label><br />
+            <input type="text" value={newTaskName} onChange={handleChange} /><br />
+            <button onClick={handleClick}>Save</button>
+        </form>
     )
 }
